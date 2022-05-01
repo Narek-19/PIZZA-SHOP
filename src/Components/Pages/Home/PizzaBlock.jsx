70bad1cefb40a63df5012e055b8d3860b81b9ce4
homepc <narekchakhoyan97@gmail.com>
2022-05-01 14:06:30 +0400

    second ui full commit

--- src/Components/Pages/Home/PizzaBlock.jsx
+++ src/Components/Pages/Home/PizzaBlock.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+import React,{useState} from "react";
+import classNames from "classnames";
 
-export const PizzaBlock = ()=>{
+export const PizzaBlock = (props)=>{
+    const [selectedT,setSelectedT] = useState(null);
+    const [selectedS,setSelectedS] = useState(null);
+    const typeNames = ["Thick","Thin"];
+    const pizzaSizes = [26,30,40];
+
+    const changeActiveItem =(index)=>{
+        setSelectedT(index);
+    }
+    const changeActiveSize = (index) => {
+        setSelectedS(index);
+    }
     return (
         <div className = "pizzaBlock">
             <div className = "pizzaImg">
                 <img src = "http://pizzahutantilles.fr/assets/images/pizza-panel-thumb.png" alt = "PizzaImage"></img>
             </div>
             <div className = "pizza-name">
-                <h2>California Pizza</h2>
+                <h2>{props.name}</h2>
             </div>
             <div className = "pizzaSettings">
                     <ul className = "pizzaTypes">
-                        <li>Type 1</li>
-                        <li>Type 2</li>
+                       {typeNames.map((type,index)=>{
+                           return(
+                           <li
+                           className = {classNames({
+                            active:index === selectedT,
+                            disabled:!props.types.includes(index)
+                           }
+                        )}
+                            onClick={() => changeActiveItem(index)}
+                           >
+                               {type}
+                            </li>
+                           )
+                       })}
                     </ul>
                     <ul className = "pizzaSize">
-                        <li>26sm</li>
-                        <li>30sm</li>
-                        <li>40sm</li>
+                        {pizzaSizes.map((size,index)=>{
+                            return ( 
+                                <li
+                                    onClick={()=> changeActiveSize(index)}
+                                className={classNames({
+                                    active:selectedS === index,
+                                    disabled:!props.sizes.includes(size)
+                                })}>{size}sm</li>
+                            )
+                        })}
                     </ul>
             </div>
             <div className = "pizza-controls">
                 <ul>
-                    <li className = "price">12$</li>
+                    <li className = "price">{props.price}$</li>
                     <li>
                         <button>
                             <span>+</span>
--- src/Components/Pages/Home/index.jsx
+++ src/Components/Pages/Home/index.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Nav } from '../../Nav';
 import { PizzaBlock } from './PizzaBlock';
 
 import "./home.scss";
 
 export const Home =()=> {
+  const[pizzaData, setPizzaData] = useState();
+
+  useEffect(()=>{
+    fetch("http://localhost:3000/db.json")
+    .then(res => res.json())
+    .then(data => setPizzaData(data.pizzas));
+  },[])
   return (
   <>
    <Nav/>
    <h2>All Pizzas</h2>
-   <PizzaBlock/>
+   <div className = "pizzaStore">
+   {
+     pizzaData && pizzaData.map((el,index)=>{
+      return(
+        <PizzaBlock key = {index} {...el}/>
+      )
+     })
+        }
+   </div>
+
   </>
   )
 }
